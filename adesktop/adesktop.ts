90a1a22f341ff37cade91e0606fd9f20b2c094fa
Focus Chen <i@focuschen.com>
2021-12-21 17:13:04 +0000

    add devStatus variable, add the capability to dynamically add publisherID prefix

--- adesktop/adesktop.ts
+++ adesktop/adesktop.ts
@@ -1,17 +1,24 @@
-function importAd(pub_id, platform) {
-    if (typeof pub_id == 'undefined' || pub_id === null) {
+function importAd(publisherID, devStatus, platform) {
+    if (typeof publisherID == 'undefined' || publisherID === null) {
         console.error('Publisher ID is undefined, the script will not run.') // throw an error if the publisher ID is undefined
     }
     else {
+        const pubid = (!publisherID.includes('ca-pub-') || publisherID.match(/^[0-9]+$/)) ? 'ca-pub-' + publisherID : publisherID;
         const adjs = 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js'; //adsense base url
-        const wholelink = adjs + '?client=' + pub_id;
+        const wholelink = adjs + '?client=' + pubid;
         const adscript = document.createElement('script');
         adscript.setAttribute('async', '');
         adscript.setAttribute('src', wholelink);
         adscript.setAttribute('crossorigin', 'anonymous'); // add attributes to the script element
         const ifMobile = /Android|BlackBerry|iPhone|iPad|iPod|webOS/i.test(navigator.userAgent); // check if the user is on a mobile device
         function runAds() {
-            console.log('ads loaded')
+            switch (devStatus) {
+                case 'dev':
+                    console.log('ads loaded with publisher ID: ' + pubid);
+                    const platformStatus = (ifMobile) ? 'mobile' : 'desktop';
+                    console.log('current platform: ' + platformStatus);
+                    break;
+            }
             document.head.appendChild(adscript); // append the script to the head of the document        
         }
         switch (platform) {
@@ -21,7 +28,9 @@ function importAd(pub_id, platform) {
                 }
                 break;
             default:
-                runAds();
+                if (ifMobile == false) {
+                    runAds();
+                }
         }
     }
 }
