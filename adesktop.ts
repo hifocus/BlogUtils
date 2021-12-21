04717549d7effbcf3252960e37552201f810a759
Focus Chen <i@focuschen.com>
2021-12-21 15:26:06 +0000

    add adesktop.ts

---
+++ adesktop.ts
@@ -0,0 +1,27 @@
+function importAd(pub_id, platform) {
+    if (typeof pub_id !== 'undefined' || pub_id === null) {
+        console.error('Publisher ID is undefined, the script will not run.') // throw an error if the publisher ID is undefined
+    }
+    else {
+        const adjs = 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js'; //adsense base url
+        const wholelink = adjs + '?client=' + pub_id;
+        const adscript = document.createElement('script');
+        adscript.setAttribute('async', '');
+        adscript.setAttribute('src', wholelink);
+        adscript.setAttribute('crossorigin', 'anonymous'); // add attributes to the script element
+        const ifMobile = /Android|BlackBerry|iPhone|iPad|iPod|webOS/i.test(navigator.userAgent); // check if the user is on a mobile device
+        function runAds() {
+            console.log('ads loaded')
+            document.head.appendChild(adscript); // append the script to the head of the document        
+        }
+        switch (platform) {
+            case 'mobile':
+                if (ifMobile == true) {
+                    runAds();
+                }
+                break;
+            default:
+                runAds();
+        }
+    }
+}
